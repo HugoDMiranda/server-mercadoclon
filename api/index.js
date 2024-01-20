506f1d35d173fe8f77e581a0fc4266830d818764
HugoDMiranda <hdmirandab@hotmail.com>
2024-01-20 00:12:29 -0500

    api error message update

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ router.get("/items", async (req, res) => {
   } catch (error) {
     res.json({
       message: "error",
+      error,
     });
   }
 });
@@ -29,6 +30,7 @@ router.get("/items/search/:id", async (req, res) => {
   } catch (error) {
     res.json({
       message: "error",
+      error,
     });
   }
 });
